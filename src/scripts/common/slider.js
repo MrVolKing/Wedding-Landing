d58ca3b8df37289db13c1b071dd561fe36cbe4de
mrKing <MrKing>
2018-10-24 14:54:56 +0300

    slidergallery

--- src/scripts/common/slider.js
+++ src/scripts/common/slider.js
@@ -2,11 +2,12 @@
 
 {
   var swiper = new Swiper('.swiper-container', {
-      navigation: {
-          nextEl: '.swiper-button-next',
-           prevEl: '.swiper-button-prev',
-       },
-       });
+    slidesPerView: 1,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    });
 };
 
 
