a0efecb0f4f7afaabcf626b61c07f2a55f31aa82
mrKing <MrKing>
2018-11-08 18:53:30 +0200

    popapsnoscroll

--- src/scripts/common/main.js
+++ src/scripts/common/main.js
@@ -1,16 +1,18 @@
 $(document).ready (function ( ) {
 
-  var sizer = '.grid-item';
+  function initMasonry () {
+    var sizer = '.grid-item';
 
-  $('.grid').masonry({
-    // options
-    itemSelector: '.grid-item',
-    columnWidth: sizer,
-    percentPosition: true
-});
+    $('.grid').masonry({
+      // options
+      itemSelector: '.grid-item',
+      columnWidth: sizer,
+      percentPosition: true
+    });
+  }
+  initMasonry();
 
-
-
-
-
+  setInterval(() => {
+    initMasonry();
+  }, 500);
 }) ;
--- src/scripts/common/popap.js
+++ src/scripts/common/popap.js
@@ -1,250 +1,176 @@
 
                                                                         // order
-    var btn_order1 = document.getElementById("btn4");
-    var btn_order2 = document.getElementById("btn5");
-    var btn_order3 = document.getElementById("btn6");
-    var btn_order4 = document.getElementById("btn7");
-    var btn_order5 = document.getElementById("btn8");
-    var btn_order6 = document.getElementById("btn9");
-    var btn_order7 = document.getElementById("btn10");
-    var openModal3 = document.getElementById("openModal3"); 
-    var close3 = document.getElementById("close3");
 
-    btn_order1.addEventListener('click', () => {
-        openModal3.classList.add('modalDialogcustomactive');
+    $('#btn8').click(function() {
+        $('.bookitpopap1').fadeIn(300);
+        $('.popup_container').css('display', 'none');
+        $('body').css('overflow','hidden');
     });
 
-    btn_order2.addEventListener('click', () => {
-        openModal3.classList.add('modalDialogcustomactive');
+    $('#btn9').click(function() {
+        $('.bookitpopap2').fadeIn(300);
+        $('.popup_container2').css('display', 'none');
+        $('body').css('overflow','hidden');
     });
 
-    btn_order3.addEventListener('click', () => {
-        openModal3.classList.add('modalDialogcustomactive');
+    $('#btn10').click(function() {
+        $('.bookitpopap3').fadeIn(300);
+        $('.popup_container3').css('display', 'none');
+        $('body').css('overflow','hidden');
     });
 
-    btn_order4.addEventListener('click', () => {
-        openModal3.classList.add('modalDialogcustomactive');
+    $('.btn4').click(function() {
+        $('.bookitpopap1').fadeIn(300);
+        $('body').css('overflow','hidden');
     });
 
-    btn_order5.addEventListener('click', () => {
-        openModal3.classList.add('modalDialogcustomactive');
+    $('.btn5').click(function() {
+        $('.bookitpopap2').fadeIn(300);
+        $('body').css('overflow','hidden');
     });
 
-    btn_order6.addEventListener('click', () => {
-        openModal3.classList.add('modalDialogcustomactive');
+    $('.btn6').click(function() {
+        $('.bookitpopap3').fadeIn(300);
+        $('body').css('overflow','hidden');
     });
 
-    btn_order7.addEventListener('click', () => {
-        openModal3.classList.add('modalDialogcustomactive');
+    $('.btn7').click(function() {
+        $('.bookitpopap4').fadeIn(300);
+        $('body').css('overflow','hidden');
     });
 
-    close3.addEventListener('click', () => {
-        openModal3.classList.remove('modalDialogcustomactive');
+    $('.closebookit1').click(function() {
+        $('.bookitpopap1').css('display', 'none');
+        $('body').css('overflow','visible');
     });
 
+    $('.closebookit2').click(function() {
+        $('.bookitpopap2').css('display', 'none');
+        $('body').css('overflow','visible');
+    });
+
+
+    $('.closebookit3').click(function() {
+        $('.bookitpopap3').css('display', 'none');
+        $('body').css('overflow','visible');
+    });
+
+    $('.closebookit4').click(function() {
+        $('.bookitpopap4').css('display', 'none');
+        $('body').css('overflow','visible');
+    });
                                                                         // call me
 
-    var btncall = document.getElementById("btncall"); 
-    var btncalladapt = document.getElementById("btncalladapt");
-    var openModa2 = document.getElementById("openModal2");
-    var close2 = document.getElementById("close2");
 
-    btncall.addEventListener('click', () => {
-        openModal2.classList.add('modalDialogcustomactive'); 
-    });  
-
-    btncalladapt.addEventListener('click', () => {
-        openModal2.classList.add('modalDialogcustomactive'); 
-    }); 
-
-    close2.addEventListener('click', () => {
-        openModal2.classList.remove('modalDialogcustomactive');
+    $('#btncall').click(function() {
+        $('#openModal2').fadeIn(300);
+        $('body').css('overflow','hidden');
     });
 
+    $('#btncalladapt').click(function() {
+        $('#openModal2').fadeIn(300);
+        $('body').css('overflow','hidden');
+    });
+
+    $('#close2').click(function() {
+        $('#openModal2').css('display', 'none');
+        $('body').css('overflow','visible');
+    });
                                                                         // contacts
 
-    var btncont = document.getElementById("btncont");
-    var openModa4 = document.getElementById("openModal4");
-    var close4 = document.getElementById("close4");
+    // $('#btncont').click(function() {
+    //     $('#openModal4').fadeIn(300);
+    //     $('body').css('overflow','hidden');
+    // });
 
-    // btncont.addEventListener('click', () => {
-    //     event.preventDefault();
-    //     openModal4.classList.add('modalDialogcustomactive');  
-    // });  
-
-    // close4.addEventListener('click', () => {
-    //     openModal4.classList.remove('modalDialogcustomactive');
+    // $('#close4').click(function() {
+    //     $('#openModal4').css('display', 'none');
+    //     $('body').css('overflow','visible');
     // });
 
                                                                             // faq modal
 
-    var block1 = document.getElementById("block1");
-    var block2 = document.getElementById("block2");
-    var block3 = document.getElementById("block3");
-    var block4 = document.getElementById("block4");
-    var block5 = document.getElementById("block5");
-    var block6 = document.getElementById("block6");
-    var block7 = document.getElementById("block7");
-    var block8 = document.getElementById("block8");
-    var block9 = document.getElementById("block9");
-    var block10 = document.getElementById("block10");
-    var block11 = document.getElementById("block11");
-    var block12 = document.getElementById("block12");
-    var block13 = document.getElementById("block13");
-    var block14 = document.getElementById("block14");
-    var block15 = document.getElementById("block15");
-    var openModal11 = document.getElementById("openModal11");
-    var openModal12 = document.getElementById("openModal12");
-    var openModal13 = document.getElementById("openModal13");
-    var openModal14 = document.getElementById("openModal14");
-    var openModal15 = document.getElementById("openModal15");
-    var openModal16 = document.getElementById("openModal16");
-    var openModal17 = document.getElementById("openModal17");
-    var openModal18 = document.getElementById("openModal18");
-    var openModal19 = document.getElementById("openModal19");
-    var openModal20 = document.getElementById("openModal20");
-    var openModal21 = document.getElementById("openModal21");
-    var openModal22 = document.getElementById("openModal22");
-    var openModal23 = document.getElementById("openModal23");
-    var openModal24 = document.getElementById("openModal24");
-    var openModal25 = document.getElementById("openModal25");
-    var close11 = document.getElementById("close11");
 
-
-    block1.addEventListener('click', () => {
-        openModal11.classList.add('modalDialogcustomactive');
+    $('#block1').click(function() {
+        $('#openModal11').fadeIn(300);
+        $('body').css('overflow','hidden');
     });
 
-    block2.addEventListener('click', () => {
-        openModal12.classList.add('modalDialogcustomactive');
+    $('#block2').click(function() {
+        $('#openModal12').fadeIn(300);
+        $('body').css('overflow','hidden');
     });
 
-    block3.addEventListener('click', () => {
-        openModal13.classList.add('modalDialogcustomactive');
+    $('#block3').click(function() {
+        $('#openModal13').fadeIn(300);
+        $('body').css('overflow','hidden');
     });
 
-    block4.addEventListener('click', () => {
-        openModal14.classList.add('modalDialogcustomactive');
+    $('#block4').click(function() {
+        $('#openModal14').fadeIn(300);
+        $('body').css('overflow','hidden');
     });
 
-    block5.addEventListener('click', () => {
-        openModal15.classList.add('modalDialogcustomactive');
+    $('#block5').click(function() {
+        $('#openModal15').fadeIn(300);
+        $('body').css('overflow','hidden');
     });
 
-    block6.addEventListener('click', () => {
-        openModal16.classList.add('modalDialogcustomactive');
+    $('#block6').click(function() {
+        $('#openModal16').fadeIn(300);
+        $('body').css('overflow','hidden');
     });
 
-    block7.addEventListener('click', () => {
-        openModal17.classList.add('modalDialogcustomactive');
+    $('#block7').click(function() {
+        $('#openModal17').fadeIn(300);
+        $('body').css('overflow','hidden');
     });
 
-    block8.addEventListener('click', () => {
-        openModal18.classList.add('modalDialogcustomactive');
+    $('#block8').click(function() {
+        $('#openModal18').fadeIn(300);
+        $('body').css('overflow','hidden');
     });
 
-    block9.addEventListener('click', () => {
-        openModal19.classList.add('modalDialogcustomactive');
+    $('#block9').click(function() {
+        $('#openModal19').fadeIn(300);
+        $('body').css('overflow','hidden');
     });
 
-    block10.addEventListener('click', () => {
-        openModal20.classList.add('modalDialogcustomactive');
+    $('#block10').click(function() {
+        $('#openModal20').fadeIn(300);
+        $('body').css('overflow','hidden');
     });
 
-    block11.addEventListener('click', () => {
-        openModal21.classList.add('modalDialogcustomactive');
+    $('#block11').click(function() {
+        $('#openModal21').fadeIn(300);
+        $('body').css('overflow','hidden');
     });
 
-    block12.addEventListener('click', () => {
-        openModal22.classList.add('modalDialogcustomactive');
+    $('#block12').click(function() {
+        $('#openModal22').fadeIn(300);
+        $('body').css('overflow','hidden');
     });
 
-    block13.addEventListener('click', () => {
-        openModal23.classList.add('modalDialogcustomactive');
+    $('#block13').click(function() {
+        $('#openModal23').fadeIn(300);
+        $('body').css('overflow','hidden');
     });
 
-    block14.addEventListener('click', () => {
-        openModal24.classList.add('modalDialogcustomactive');
+    $('#block14').click(function() {
+        $('#openModal24').fadeIn(300);
+        $('body').css('overflow','hidden');
     });
 
-    block15.addEventListener('click', () => {
-        openModal25.classList.add('modalDialogcustomactive');
+    $('#block15').click(function() {
+        $('#openModal25').fadeIn(300);
+        $('body').css('overflow','hidden');
     });
 
-    close11.addEventListener('click', () => {
-        openModal11.classList.remove('modalDialogcustomactive');
-        
+    $('.close1').click(function() {
+        $('.closemodal').css('display', 'none');
+        $('body').css('overflow','visible');
     });
 
-    close19.addEventListener('click', () => {
-        openModal19.classList.remove('modalDialogcustomactive');
-        
-    });
-
-    close20.addEventListener('click', () => {
-        openModal20.classList.remove('modalDialogcustomactive');
-        
-    });
-
-    close12.addEventListener('click', () => {
-        openModal12.classList.remove('modalDialogcustomactive');
-        
-    });
-
-    close13.addEventListener('click', () => {
-        openModal13.classList.remove('modalDialogcustomactive');
-        
-    });
-
-    close14.addEventListener('click', () => {
-        openModal14.classList.remove('modalDialogcustomactive');
-        
-    });
-
-    close15.addEventListener('click', () => {
-        openModal15.classList.remove('modalDialogcustomactive');
-        
-    });
-
-    close16.addEventListener('click', () => {
-        openModal16.classList.remove('modalDialogcustomactive');
-        
-    });
-
-    close17.addEventListener('click', () => {
-        openModal17.classList.remove('modalDialogcustomactive');
-        
-    });
-
-    close18.addEventListener('click', () => {
-        openModal18.classList.remove('modalDialogcustomactive');
-        
-    });
-
-    close21.addEventListener('click', () => {
-        openModal21.classList.remove('modalDialogcustomactive');
-        
-    });
-
-    close22.addEventListener('click', () => {
-        openModal22.classList.remove('modalDialogcustomactive');
-        
-    });
-
-    close23.addEventListener('click', () => {
-        openModal23.classList.remove('modalDialogcustomactive');
-        
-    });
-
-    close24.addEventListener('click', () => {
-        openModal24.classList.remove('modalDialogcustomactive');
-        
-    });
-
-    close25.addEventListener('click', () => {
-        openModal25.classList.remove('modalDialogcustomactive');
-        
-    });
 
                                                                             // more info modal
 
@@ -278,3 +204,15 @@
         $('body').css('overflow','visible');
     });
     
+
+                                                                // header menu
+
+    $('.header__menu a').click(function() {
+        event.preventDefault();
+        var e = $(this).attr("href"),
+        t = $(e).offset().top;
+        $("body,html").animate({
+            scrollTop: t
+        }, 1500)
+
+    });
