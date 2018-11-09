c5a0fe67a707d92bc3869cf01676f4c02dac5539
mrKing <MrKing>
2018-11-09 16:52:38 +0200

    editspopapsscroll

--- src/scripts/common/popap.js
+++ src/scripts/common/popap.js
@@ -4,77 +4,91 @@
     $('#btn8').click(function() {
         $('.bookitpopap1').fadeIn(300);
         $('.popup_container').css('display', 'none');
-        $('body').css('overflow','hidden');
+        // $('body').css('overflow','hidden');
+        $('.openmodal').addClass('openmodalactive');
     });
 
     $('#btn9').click(function() {
         $('.bookitpopap2').fadeIn(300);
         $('.popup_container2').css('display', 'none');
-        $('body').css('overflow','hidden');
+        // $('body').css('overflow','hidden');
+        $('.openmodal').addClass('openmodalactive');
     });
 
     $('#btn10').click(function() {
         $('.bookitpopap3').fadeIn(300);
         $('.popup_container3').css('display', 'none');
-        $('body').css('overflow','hidden');
+        // $('body').css('overflow','hidden');
+        $('.openmodal').addClass('openmodalactive');
     });
 
     $('.btn4').click(function() {
         $('.bookitpopap1').fadeIn(300);
-        $('body').css('overflow','hidden');
+        // $('body').css('overflow','hidden');
+        $('.openmodal').addClass('openmodalactive');
     });
 
     $('.btn5').click(function() {
         $('.bookitpopap2').fadeIn(300);
-        $('body').css('overflow','hidden');
+        // $('body').css('overflow','hidden');
+        $('.openmodal').addClass('openmodalactive');
     });
 
     $('.btn6').click(function() {
         $('.bookitpopap3').fadeIn(300);
-        $('body').css('overflow','hidden');
+        // $('body').css('overflow','hidden');
+        $('.openmodal').addClass('openmodalactive');
     });
 
     $('.btn7').click(function() {
         $('.bookitpopap4').fadeIn(300);
-        $('body').css('overflow','hidden');
+        $('.openmodal').addClass('openmodalactive');
+        // $('.content').css('overflow','hidden').css('height', '100vh');
     });
 
     $('.closebookit1').click(function() {
         $('.bookitpopap1').css('display', 'none');
-        $('body').css('overflow','visible');
+        // $('body').css('overflow','visible');
+        $('.openmodal').removeClass('openmodalactive');
     });
 
     $('.closebookit2').click(function() {
         $('.bookitpopap2').css('display', 'none');
-        $('body').css('overflow','visible');
+        // $('body').css('overflow','visible');
+        $('.openmodal').removeClass('openmodalactive');
     });
 
 
     $('.closebookit3').click(function() {
         $('.bookitpopap3').css('display', 'none');
-        $('body').css('overflow','visible');
+        // $('body').css('overflow','visible');
+        $('.openmodal').removeClass('openmodalactive');
     });
 
     $('.closebookit4').click(function() {
         $('.bookitpopap4').css('display', 'none');
-        $('body').css('overflow','visible');
+        // $('body').css('overflow','visible');
+        $('.openmodal').removeClass('openmodalactive');
     });
                                                                         // call me
 
 
     $('#btncall').click(function() {
         $('#openModal2').fadeIn(300);
-        $('body').css('overflow','hidden');
+        // $('body').css('overflow','hidden');
+        $('.openmodal').addClass('openmodalactive');
     });
 
     $('#btncalladapt').click(function() {
         $('#openModal2').fadeIn(300);
-        $('body').css('overflow','hidden');
+        // $('body').css('overflow','hidden');
+        $('.openmodal').addClass('openmodalactive');
     });
 
     $('#close2').click(function() {
         $('#openModal2').css('display', 'none');
-        $('body').css('overflow','visible');
+        // $('body').css('overflow','visible');
+        $('.openmodal').removeClass('openmodalactive');
     });
                                                                         // contacts
 
@@ -93,82 +107,98 @@
 
     $('#block1').click(function() {
         $('#openModal11').fadeIn(300);
-        $('body').css('overflow','hidden');
+        // $('body').css('overflow','hidden');
+        $('.openmodal').addClass('openmodalactive');
     });
 
     $('#block2').click(function() {
         $('#openModal12').fadeIn(300);
-        $('body').css('overflow','hidden');
+        // $('body').css('overflow','hidden');
+        $('.openmodal').addClass('openmodalactive');
     });
 
     $('#block3').click(function() {
         $('#openModal13').fadeIn(300);
-        $('body').css('overflow','hidden');
+        // $('body').css('overflow','hidden');
+        $('.openmodal').addClass('openmodalactive');
     });
 
     $('#block4').click(function() {
         $('#openModal14').fadeIn(300);
-        $('body').css('overflow','hidden');
+        // $('body').css('overflow','hidden');
+        $('.openmodal').addClass('openmodalactive');
     });
 
     $('#block5').click(function() {
         $('#openModal15').fadeIn(300);
-        $('body').css('overflow','hidden');
+        // $('body').css('overflow','hidden');
+        $('.openmodal').addClass('openmodalactive');
     });
 
     $('#block6').click(function() {
         $('#openModal16').fadeIn(300);
-        $('body').css('overflow','hidden');
+        // $('body').css('overflow','hidden');
+        $('.openmodal').addClass('openmodalactive');
     });
 
     $('#block7').click(function() {
         $('#openModal17').fadeIn(300);
-        $('body').css('overflow','hidden');
+        // $('body').css('overflow','hidden');
+        $('.openmodal').addClass('openmodalactive');
     });
 
     $('#block8').click(function() {
         $('#openModal18').fadeIn(300);
-        $('body').css('overflow','hidden');
+        // $('body').css('overflow','hidden');
+        $('.openmodal').addClass('openmodalactive');
     });
 
     $('#block9').click(function() {
         $('#openModal19').fadeIn(300);
-        $('body').css('overflow','hidden');
+        // $('body').css('overflow','hidden');
+        $('.openmodal').addClass('openmodalactive');
     });
 
     $('#block10').click(function() {
         $('#openModal20').fadeIn(300);
-        $('body').css('overflow','hidden');
+        // $('body').css('overflow','hidden');
+        $('.openmodal').addClass('openmodalactive');
     });
 
     $('#block11').click(function() {
         $('#openModal21').fadeIn(300);
-        $('body').css('overflow','hidden');
+        // $('body').css('overflow','hidden');
+        $('.openmodal').addClass('openmodalactive');
     });
 
     $('#block12').click(function() {
         $('#openModal22').fadeIn(300);
-        $('body').css('overflow','hidden');
+        // $('body').css('overflow','hidden');
+        $('.openmodal').addClass('openmodalactive');
     });
 
     $('#block13').click(function() {
         $('#openModal23').fadeIn(300);
-        $('body').css('overflow','hidden');
+        // $('body').css('overflow','hidden');
+        $('.openmodal').addClass('openmodalactive');
     });
 
     $('#block14').click(function() {
         $('#openModal24').fadeIn(300);
-        $('body').css('overflow','hidden');
+        // $('body').css('overflow','hidden');
+        $('.openmodal').addClass('openmodalactive');
     });
 
     $('#block15').click(function() {
         $('#openModal25').fadeIn(300);
-        $('body').css('overflow','hidden');
+        // $('body').css('overflow','hidden');
+        $('.openmodal').addClass('openmodalactive');
     });
 
     $('.close1').click(function() {
         $('.closemodal').css('display', 'none');
-        $('body').css('overflow','visible');
+        // $('body').css('overflow','visible');
+        $('.openmodal').removeClass('openmodalactive');
     });
 
 
@@ -176,32 +206,38 @@
 
     $('.elem').click(function() {
         $('.popup_container').fadeIn(300);
-        $('body').css('overflow','hidden');
+        // $('body').css('overflow','hidden');
+        $('.openmodal').addClass('openmodalactive');
     });
     
     $('.closejq').click(function(){
         $('.popup_container').css('display', 'none');
-        $('body').css('overflow','visible');
+        // $('body').css('overflow','visible');
+        $('.openmodal').removeClass('openmodalactive');
     });
 
     $('.elem2').click(function() {
         $('.popup_container2').fadeIn(300);
-        $('body').css('overflow','hidden');
+        // $('body').css('overflow','hidden');
+        $('.openmodal').addClass('openmodalactive');
     });
     
     $('.closejq2').click(function(){
         $('.popup_container2').css('display', 'none');
-        $('body').css('overflow','visible');
+        // $('body').css('overflow','visible');
+        $('.openmodal').removeClass('openmodalactive');
     });
 
     $('.elem3').click(function() {
         $('.popup_container3').fadeIn(300);
-        $('body').css('overflow','hidden');
+        // $('body').css('overflow','hidden');
+        $('.openmodal').addClass('openmodalactive');
     });
     
     $('.closejq3').click(function(){
         $('.popup_container3').css('display', 'none');
-        $('body').css('overflow','visible');
+        // $('body').css('overflow','visible');
+        $('.openmodal').removeClass('openmodalactive');
     });
     
 
